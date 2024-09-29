3c6f91a75c626d6a6488c6e6a8c76deb6150863c
João Victor Carvalho <joaovictor.crvl@gmail.com>
2024-09-29 11:00:53 -0300

    file update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import { headerColor } from './src/headerColor.js';
-import { cursor } from './src/headerColor.js';
+import { headerColor } from './headerColor.js';
+import { cursor } from './headerColor.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     headerColor(); // Chama a função para adicionar o listener de scroll
